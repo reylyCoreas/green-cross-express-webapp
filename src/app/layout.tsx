9c55c6a49e088b3f8ea1a13e8b0f235d72c46257
reylyCoreas <142835511+reylyCoreas@users.noreply.github.com>
2025-12-10 18:36:25 -0600

    fixing metadata errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,26 +4,13 @@ import { Metadata } from "next";
 import ClientProviders from "../components/ClientProviders";
 import Navbar from "../components/Navbar";
 import CartDrawer from "../components/CartDrawer";
-<<<<<<< HEAD
 import AgeGate from "../components/AgeGate";
 import { Geist, Geist_Mono } from "next/font/google";
-=======
-import { CartProvider } from "../context/CartContext";
-import { Geist } from "next/font/google";
-import { Geist_Mono } from "next/font/google";
-import React from "react";
-import ClientProviders from "../components/ClientProviders";
->>>>>>> eddd6309d9a27edafedbababa35f3df9344bee64
 
 //Move all client-only logic into a client wrapper and mark AgeGate as client. 
-<<<<<<< HEAD
 
 export const metadata: Metadata = {
   title: "GreenCross – Premium Cannabis PreOrdere & Pickup in Houston",
-=======
-export const metadata: MetaData = {
-  title: "GreenCross – Premium Cannabis PreOrdred",
->>>>>>> eddd6309d9a27edafedbababa35f3df9344bee64
   description:
     "Browse curated cannabis products, pre-order online, and pick up at your nearest GreenCross location in Houston.",
 };
@@ -38,16 +25,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-<<<<<<< HEAD
-=======
-// Metadata
-export const metadata: Metadata = {
-  title: "GreenCross – Premium Cannabis PreOrdred",
-  description:
-    "Browse curated cannabis products, pre-order online, and pick up at your nearest GreenCross location in Houston.",
-};
-
->>>>>>> eddd6309d9a27edafedbababa35f3df9344bee64
 export default function RootLayout({
   children,
 }: {
